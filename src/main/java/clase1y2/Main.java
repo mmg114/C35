a38bafeec0f3a5a7b2d67ac20644b4042b64b523
Mauricio-Munoz <mauricio.m@moabits.com>
2025-09-09 20:25:33 -0500

    primer commit

---
+++ src/main/java/clase1y2/Main.java
@@ -0,0 +1,40 @@
+package clase1y2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.print("Hola mundo-");
+        System.out.print("Hola mundo");
+
+        //Tipos de datos
+        //Numerico
+        short numero = 1234;
+        byte b = 127;
+        int a = 25/18;
+        long id = 1234567890;
+        // Numerico .decimal
+        double altura = 1.75;
+        float peso = 75.5f;
+        //Caracteres
+        char letra = 'a';
+        String nombre = "Hola mi nombre es Mauricio";
+        String parrago = """
+                
+                Hola
+                Mi nombre es Mauricio
+                
+                """;
+        //Booleanos
+        boolean comparacionUno = 3<22;
+        int edadPrimitiva = 55;
+        Integer edadObjeto = null;
+        Long amount = null;
+        Byte bObjeto = null;
+        Character letraObjeto = 'a';
+        Double alturaObjeto = 1.75;
+        Float pesoObjeto = 75.5f;
+        Boolean comparacionUnoObjeto = null;
+        Short numeroObjeto = null;
+    }
+}
---
+++ src/main/java/clase3/Main.java
@@ -0,0 +1,25 @@
+package clase3;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //opcion 1 null
+      //  Scanner scanner=null;
+        //opcion 2 inicializar el objeto
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre");
+        String nombre = scanner.nextLine();
+        System.out.println("Imp..."+nombre);
+
+        System.out.println("Ingrese su edad");
+        int edad = scanner.nextInt();
+        System.out.println("Imp..."+edad);
+
+        scanner.close();
+    }
+
+}
